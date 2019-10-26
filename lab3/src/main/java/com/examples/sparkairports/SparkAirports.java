67501a773b5bcba7a453fb0d39c35fb810f8aeb2
nika <pinskaya.nika@mail.ru>
2019-10-26 10:55:03 +0300

    Scripted auto-commit on change (2019-10-26 10:55:03) by gitwatch.sh

--- lab3/src/main/java/com/examples/sparkairports/SparkAirports.java
+++ lab3/src/main/java/com/examples/sparkairports/SparkAirports.java
@@ -1,7 +1,14 @@
 package com.examples.sparkairports;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class SparkAirports {
     public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName("lab3doubl");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+
 
     }
 }
