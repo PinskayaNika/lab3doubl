98bce2a3fe16cd6700ec9bce00a118757fcb7dba
nika <pinskaya.nika@mail.ru>
2019-11-02 00:36:05 +0300

    Scripted auto-commit on change (2019-11-02 00:36:05) by gitwatch.sh

--- lab3/src/main/java/com/examples/sparkairports/FlightFunctions.java
+++ lab3/src/main/java/com/examples/sparkairports/FlightFunctions.java
@@ -9,7 +9,7 @@ public class FlightFunctions {
     private static final int AIRPORT_ID_TO_POS = 14;
     private static final int AIRPORT_ID_FROM_POS = 11;
     private static final int DELAY_FLIGHT_POS = 17;
-S    private static final int IS_CANCELLED_POS = 1;
+    private static final int IS_CANCELLED_POS = 1;
     private static final int NUMBER_OF_DELAY_POS = 2;
     private static final int NUMBER_OF_CANCELLED_POS = 3;
     private static final int NUMBER_OF_FLIGHTS_POS = 4;
