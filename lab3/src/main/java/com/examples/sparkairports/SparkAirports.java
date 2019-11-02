535d8660e33faee75e0ea7d973fa4015d4a731b8
nika <pinskaya.nika@mail.ru>
2019-11-02 13:04:28 +0300

    Scripted auto-commit on change (2019-11-02 13:04:28) by gitwatch.sh

--- lab3/src/main/java/com/examples/sparkairports/SparkAirports.java
+++ lab3/src/main/java/com/examples/sparkairports/SparkAirports.java
@@ -97,7 +97,7 @@ public class SparkAirports {
         final String header = data.first();
         return data.filter(line -> !line.equals(header));
     }
-
+//
     private static JavaRDD<String> mapAirportsIDs(JavaPairRDD<Pair<Integer, Integer>, String> schedule, final Broadcast<Map<Integer,String>> airportsBroadcasted){
         return schedule.map(data -> {
             int airportID1 = data._1.getKey();
