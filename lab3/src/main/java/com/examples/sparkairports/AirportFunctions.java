97d63adbdc1e8b4d895caa21742b12bc5fbff4f5
nika <pinskaya.nika@mail.ru>
2019-11-02 12:49:43 +0300

    Scripted auto-commit on change (2019-11-02 12:49:43) by gitwatch.sh

--- lab3/src/main/java/com/examples/sparkairports/AirportFunctions.java
+++ lab3/src/main/java/com/examples/sparkairports/AirportFunctions.java
@@ -1,39 +1,4 @@
 package com.examples.sparkairports;
-//
-//import org.apache.spark.api.java.JavaPairRDD;
-//import org.apache.spark.api.java.JavaRDD;
-//import org.apache.spark.api.java.JavaSparkContext;
-//import org.apache.spark.broadcast.Broadcast;
-//import scala.Tuple2;
-//
-//import java.util.Map;
-//
-//public class AirportFunctions {
-//    private static final int AIRPORT_ID_POS = 0;
-//    private static final int AIRPORT_NAME_POS = 1;
-//
-//    public static Broadcast<Map<Integer, String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airport) {
-//        JavaPairRDD<Integer, String> airportPair = airport.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)), getAirportData(AIRPORT_NAME_POS, s, true)));
-//        Map<Integer, String> airportsMap = airportPair.collectAsMap();
-//                return sc.broadcast(airportsMap);
-//
-//    }
-//
-//    public static String getAirportData(int pos, String s, boolean isAirport) {
-//        if (isAirport) {
-//            return getFromAirport(pos, s);
-//        } else {
-//            return FlightFunctions.getFromFlight(pos, s);
-//        }
-//    }
-//
-//
-//    private static String getFromAirport(int pos, String s) {
-//        String strsub = s.split(",", 2)[pos];
-//        return strsub.substring(1, strsub.length() - 1);
-//    }
-//}
-
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -44,25 +9,27 @@ import scala.Tuple2;
 import java.util.Map;
 
 public class AirportFunctions {
-    private static final int AIRPORT_ID_POS= 0;
+    private static final int AIRPORT_ID_POS = 0;
     private static final int AIRPORT_NAME_POS = 1;
 
-    public static Broadcast<Map<Integer,String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airports){
-        JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)), getAirportData(AIRPORT_NAME_POS, s, true)));
-        Map<Integer, String> airportsMap = airportsPair.collectAsMap();
-        return sc.broadcast(airportsMap);
+    public static Broadcast<Map<Integer, String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airport) {
+        JavaPairRDD<Integer, String> airportPair = airport.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)), getAirportData(AIRPORT_NAME_POS, s, true)));
+        Map<Integer, String> airportsMap = airportPair.collectAsMap();
+                return sc.broadcast(airportsMap);
+
     }
 
-    public static String getAirportData(int pos, String s, boolean isAirports){
-        if (isAirports){
-            return getFromAirports(pos, s);
+    public static String getAirportData(int pos, String s, boolean isAirport) {
+        if (isAirport) {
+            return getFromAirport(pos, s);
         } else {
-            return FlightFunctions.getFromSchedule(pos, s);
+            return FlightFunctions.getFromFlight(pos, s);
         }
     }
 
-    private static String getFromAirports(int pos, String s){
-        String s_sub = s.split(",", 2)[pos];
-        return s_sub.substring(1,s_sub.length() - 1);
+
+    private static String getFromAirport(int pos, String s) {
+        String strsub = s.split(",", 2)[pos];
+        return strsub.substring(1, strsub.length() - 1);
     }
 }
