7e8a3fac677fe43b503eb483a483ff20b005dd1b
nika <pinskaya.nika@mail.ru>
2019-11-09 11:19:03 +0300

    Scripted auto-commit on change (2019-11-09 11:19:03) by gitwatch.sh

--- lab3/src/main/java/com/examples/sparkairports/AirportFunctions.java
+++ lab3/src/main/java/com/examples/sparkairports/AirportFunctions.java
@@ -8,11 +8,11 @@ import scala.Tuple2;
 
 import java.util.Map;
 
-public class AirportFunctions {
+class AirportFunctions {
     private static final int AIRPORT_ID_POS = 0;
     private static final int AIRPORT_NAME_POS = 1;
 
-    public static Broadcast<Map<Integer, String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airport) {
+    static Broadcast<Map<Integer, String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airport) {
         //Преобразуем RDD в RDD пару ключ значение с помощью метода mapToPair
         //В качестве ключа для пары аэропортов используем класс Tuple2
         JavaPairRDD<Integer, String> airportPair = airport.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)),
@@ -24,7 +24,7 @@ public class AirportFunctions {
 
     }
 
-    public static String getAirportData(int pos, String s, boolean isAirport) {
+    static String getAirportData(int pos, String s, boolean isAirport) {
         if (isAirport) {
             return getFromAirport(pos, s);
         } else {
--- lab3/src/main/java/com/examples/sparkairports/FlightFunctions.java
+++ lab3/src/main/java/com/examples/sparkairports/FlightFunctions.java
@@ -5,7 +5,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import scala.Tuple2;
 
-public class FlightFunctions {
+class FlightFunctions {
     private static final int MAX_DELAY_POS = 0;
     private static final int AIRPORT_ID_TO_POS = 14;
     private static final int AIRPORT_ID_FROM_POS = 11;
@@ -15,16 +15,15 @@ public class FlightFunctions {
     private static final int NUMBER_OF_CANCELLED_POS = 3;
     private static final int NUMBER_OF_FLIGHTS_POS = 4;
 
-    public static String getFromFlight(int pos, String s) {
+    static String getFromFlight(int pos, String s) {
         return s.split(",")[pos];
     }
 
-    public static JavaPairRDD<Pair<Integer, Integer>, String> handleFlight(JavaRDD<String> flights) {
+    static JavaPairRDD<Pair<Integer, Integer>, String> handleFlight(JavaRDD<String> flights) {
         JavaPairRDD<Pair<Integer, Integer>, float[]> flightPair = createFlightsPair(flights);
         JavaPairRDD<Pair<Integer, Integer>, float[]> flightFiltered = filterFlights(flightPair);
         JavaPairRDD<Pair<Integer, Integer>, float[]> flightReduced = reduceFlights(flightFiltered);
-        JavaPairRDD<Pair<Integer, Integer>, String> flightStringConverted = dataToString(flightReduced);
-        return flightStringConverted;
+        return dataToString(flightReduced);
     }
 
     private static JavaPairRDD<Pair<Integer, Integer>, float[]> createFlightsPair (JavaRDD<String> flights) {
